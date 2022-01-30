0a09413ddfb43b5b8548eb97350673771a2d9f1e
d4nkmemed4wg <jakob.hengstler@outlook.com>
2022-01-31 00:41:16 +0100

    added basic telnet chat for uno

--- uno/src/main.cpp
+++ uno/src/main.cpp
@@ -2,11 +2,60 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+byte mac[] = {0x00, 0xAA, 0xBB,0xCC,0xDE,0x02};
+IPAddress ip(192,168,0,119);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+EthernetServer server(23);
+boolean gotAMessage = false;
+
+String message = "";
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial){
+
+  }
+
+  Ethernet.begin(mac,ip,gateway,subnet);
+  Serial.print("My IP adress: ");
+  ip = Ethernet.localIP();
+  for (byte thisByte = 0;thisByte < 4;thisByte++){
+    Serial.print(ip[thisByte],DEC);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  EthernetClient client = server.available();
+
+  if (client){
+    if (!gotAMessage){
+      Serial.println("->New Client");
+      client.println("Hello");
+      gotAMessage = true;
+    }
+
+    char thisChar = client.read();
+
+    if (thisChar == '\r'){
+      if(message.indexOf("disconnect") > -1){
+        Serial.println("->Client closed connection");
+        server.write(":( \n");
+        client.stop();
+        message = "";
+      }else{
+        server.write("received \n");
+        Serial.println(message);
+        message = "";
+      }
+    }else{
+      message += thisChar;
+    }
+
+  }
 }
